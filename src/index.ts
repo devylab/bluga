6c044bdba57db58a7d37451d433ce0dbff4c7d27
Cavdy <cavdyofficials@gmail.com>
2022-11-17 21:00:01 +0100

    database and cache setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import fastify, { FastifyInstance } from 'fastify';
+import fastify from 'fastify';
 import { env } from '@shared/constants/env';
 import { logger } from '@shared/logger';
+import database from '@shared/database';
 import { AppModule } from './app.module';
+import cache from '@shared/cache';
 
 export class AppInstance {
-  private readonly server: FastifyInstance;
+  private readonly server;
 
   constructor() {
     this.server = fastify({
@@ -15,8 +17,18 @@ export class AppInstance {
   }
 
   async startApp() {
+    await database.connect();
     const appModule = new AppModule(this.server);
     appModule.loadRoutes();
+
+    this.server.setErrorHandler(function (error, request, reply) {
+      if (error instanceof fastify.errorCodes.FST_ERR_BAD_STATUS_CODE) {
+        // Log error
+        this.log.error(error);
+        // Send error response
+        reply.status(500).send({ ok: false });
+      }
+    });
 
     this.server.listen({ port: env.port }, (err, address) => {
       if (err) {
@@ -29,15 +41,24 @@ export class AppInstance {
   }
 
   async stopApp() {
+    await database.disconnect();
+    await cache.close();
     await this.server.close();
     logger.info('app stopped successfully');
+    process.exit(1);
   }
 }
 
 const app = new AppInstance();
-app.startApp();
+app.startApp().catch((err) => {
+  app.stopApp();
+  logger.error(err, 'app crashed on start');
+});
 
 const events = ['exit', 'SIGINT', 'SIGUSR1', 'SIGUSR2', 'SIGTERM', 'uncaughtException'];
 events.forEach((event) => {
-  process.on(event, () => app.stopApp());
+  process.on(event, () => {
+    app.stopApp();
+    logger.info(`app stopped because this event: ${event} was triggere`);
+  });
 });
