3b1a1b609fb445ddeb04d05a96352256f6f5e313
Cavdy <cavdyofficials@gmail.com>
2022-11-19 12:33:13 +0100

    admin page nextjs

---
+++ src/pages/admin/login.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Login = () => {
+  return <div>Hello this is admin login</div>;
+};
+
+export default Login;
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,5 +1,7 @@
 import { nanoid } from 'nanoid';
 import argon2 from 'argon2';
+import { BehaviorSubject } from 'rxjs';
+import { AdminRoute } from './utils.interface';
 
 export class Utils {
   static uniqueId(size = 10) {
@@ -12,5 +14,28 @@ export class Utils {
 
   static async comparePassword(hash: string, password: string) {
     return argon2.verify(hash, password);
+  }
+
+  static renderAdminRoutes() {
+    const routes = <AdminRoute[]>[
+      {
+        route: '/admin/login',
+      },
+    ];
+    const adminRoutes = new BehaviorSubject<AdminRoute[]>(routes);
+
+    const addNewAdminRoute = function (item: AdminRoute | AdminRoute[]) {
+      const newRoutes = [...adminRoutes.value];
+
+      if (Array.isArray(item)) {
+        newRoutes.concat(item);
+      } else {
+        newRoutes.push(item);
+      }
+
+      adminRoutes.next(newRoutes);
+    };
+
+    return { addNewAdminRoute, adminRoutes };
   }
 }
