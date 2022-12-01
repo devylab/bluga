31ddc57bdea6354224d04faf1cb096638bc3ab98
Cavdy <cavdyofficials@gmail.com>
2022-12-01 21:40:01 +0100

    Add editor

---
+++ public/scripts/editor.js
@@ -0,0 +1,33 @@
+window.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('appearance')?.addEventListener('click', () => {
+    console.log('headache');
+    if (
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      console.log('\n', localStorage.theme);
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+
+    // Whenever the user explicitly chooses light mode
+    // localStorage.theme = 'light';
+
+    // // Whenever the user explicitly chooses dark mode
+    // localStorage.theme = 'dark';
+
+    // // Whenever the user explicitly chooses to respect the OS preference
+    // localStorage.removeItem('theme');
+  });
+});
+
+const editor = new EditorJS({
+  holder: 'editorjs',
+  placeholder: 'Let`s write an awesome story!',
+});
+
+const onSubmit = async () => {
+  const outputData = await editor.save();
+  console.log('OUTPUT DATA', outputData);
+};
